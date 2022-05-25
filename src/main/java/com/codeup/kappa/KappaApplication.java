e5cd60308473e3fa49ac34c9089b630fdeb26a3b
John Castro <johhncaastro@gmail.com>
2022-05-25 16:39:21 -0500

    initial push

---
+++ src/main/java/com/codeup/kappa/KappaApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.kappa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KappaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KappaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/kappa/KappaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.kappa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KappaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
