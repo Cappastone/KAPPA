8552612a50376fbb539d86faaad7a80ff0b61a72
Christopher Berry <christopher.m.berry01@gmail.com>
2022-06-02 16:58:38 -0500

    add to seeder

--- src/main/java/com/codeup/kappa/controllers/GameController.java
+++ src/main/java/com/codeup/kappa/controllers/GameController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class GameController {
 
     private final GameRepository GameDao;
+
     public GameController(GameRepository GameDao) {
         this.GameDao = GameDao;
     }
@@ -39,5 +40,7 @@ public class GameController {
         GameDao.save(game);
         return "games/rawg";
     }
+
+
 
 }
