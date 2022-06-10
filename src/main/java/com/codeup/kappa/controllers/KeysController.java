14e6ca683c895abaca59d43d795245fd9bb80aad
John Castro <johhncaastro@gmail.com>
2022-06-10 11:39:01 -0500

    add rapid api key to controller

--- src/main/java/com/codeup/kappa/controllers/KeysController.java
+++ src/main/java/com/codeup/kappa/controllers/KeysController.java
@@ -12,11 +12,21 @@ public class KeysController {
     @Value("token")
     private String token;
 
+    @Value("RAPID_API_TOKEN")
+    private String RAPID_API_TOKEN;
+
     @GetMapping(path = "/keys", produces = "application/javascript")
     @ResponseBody
     public String keys() {
         String rawg = "const token =" + token;
         return rawg;
+    }
+
+    @GetMapping(path = "/keys", produces = "application/javascript")
+    @ResponseBody
+    public String key() {
+        String rapidApiToken = "const RAPID_API_TOKEN=" + RAPID_API_TOKEN;
+        return rapidApiToken;
     }
 
 }
