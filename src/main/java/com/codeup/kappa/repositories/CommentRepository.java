dc9b987590de5397f39d81636b539d991b3643a0
Oscar Castro <oscar.a.castro818@gmail.com>
2022-06-13 12:43:04 -0500

    feat: added delete post feature with help from Trevor Mullis

--- src/main/java/com/codeup/kappa/repositories/CommentRepository.java
+++ src/main/java/com/codeup/kappa/repositories/CommentRepository.java
@@ -2,7 +2,12 @@ package com.codeup.kappa.repositories;
 
 import com.codeup.kappa.models.Comment;
 import com.codeup.kappa.models.Game;
+import com.codeup.kappa.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+//    void deleteCommentByPost(Post post);
+
+    void deleteAllByPost(Post post);
+
 }
--- src/main/java/com/codeup/kappa/repositories/PostImageRepository.java
+++ src/main/java/com/codeup/kappa/repositories/PostImageRepository.java
@@ -1,5 +1,6 @@
 package com.codeup.kappa.repositories;
 
+import com.codeup.kappa.models.Post;
 import com.codeup.kappa.models.PostImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,6 @@ import java.util.List;
 public interface PostImageRepository extends JpaRepository<PostImage, Long> {
 
 //    List<PostImage> getPostImageByPostId
+
+    void deleteAllByPost(Post post);
 }
