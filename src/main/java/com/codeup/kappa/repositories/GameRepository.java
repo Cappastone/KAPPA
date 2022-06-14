513164d30fe1cbf30301e016364ed21e4fe12ea1
John Castro <johhncaastro@gmail.com>
2022-06-14 10:25:45 -0500

    fix: database name hardcoded incorrectly

--- src/main/java/com/codeup/kappa/repositories/GameRepository.java
+++ src/main/java/com/codeup/kappa/repositories/GameRepository.java
@@ -9,25 +9,24 @@ import java.util.List;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
 
-    @Query(value = "SELECT game_id FROM gamer_haven_db.user_games t GROUP BY t.game_id ORDER BY COUNT(*) DESC;", nativeQuery = true)
+    @Query(value = "SELECT game_id FROM gamerhaven_db.user_games t GROUP BY t.game_id ORDER BY COUNT(*) DESC;", nativeQuery = true)
     List<String> findGameByMostLiked();
 
 //    Game getGameByGamesApiId(long id);
 
 
-
     Game getGameById(long id);
 
 
-    @Query(value = "SELECT * FROM gamer_haven_db.games g WHERE g.id IN :ids", nativeQuery = true)
-    List <Game> findGamesByIds(List <Long> ids);
+    @Query(value = "SELECT * FROM gamerhaven_db.games g WHERE g.id IN :ids", nativeQuery = true)
+    List<Game> findGamesByIds(List<Long> ids);
 
-    @Query(value = "SELECT * FROM gamer_haven_db.games g WHERE g.games_api_id = :api_id", nativeQuery = true)
+    @Query(value = "SELECT * FROM gamerhaven_db.games g WHERE g.games_api_id = :api_id", nativeQuery = true)
     Game findGameByGamesApiId(long api_id);
 //    List<Game> getGameByUserId(long id);
 
-    @Query(value = "SELECT games_api_id FROM gamer_haven_db.games", nativeQuery = true)
-    List <Long> findAllApiIds();
+    @Query(value = "SELECT games_api_id FROM gamerhaven_db.games", nativeQuery = true)
+    List<Long> findAllApiIds();
 
 
 }
