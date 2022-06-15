dc0af5bcf386d2d6c21bbbdff3aaab84186d2cf4
Christopher Berry <christopher.m.berry01@gmail.com>
2022-06-15 14:15:59 -0500

    add date formatting to controllers and views

--- src/main/java/com/codeup/kappa/controllers/DiscoverController.java
+++ src/main/java/com/codeup/kappa/controllers/DiscoverController.java
@@ -34,19 +34,6 @@ public class DiscoverController {
         this.commentDao = commentDao;
     }
 
-//    HttpServletRequest session;
-//
-//    public HttpSession getSession() {
-//        try {
-//            return session.getSession(false);
-//        } catch (NullPointerException e) {
-//            System.out.println("User not authenticated");
-//
-//        }
-//
-//        return null;
-//    }
-
     @GetMapping
     public String topPostsAndGames(Model model) {
 
@@ -77,6 +64,13 @@ public class DiscoverController {
 
             model.addAttribute("newComment", new Comment());
         }
+
+        //        get array list of dates in desired string format =>
+        List<Date> postCreationDateObjs = getPostDateObjs(mostLikedPosts);
+        List<String> postDates = getDates(postCreationDateObjs);
+
+        model.addAttribute("postCreationDates", postDates);
+
         return "index/discover";
     }
 
@@ -123,6 +117,59 @@ public class DiscoverController {
         commentDao.save(newComment);
 
         return "redirect:/discover";
+    }
+
+    public static String getDate(Date date) {
+
+        long diff = System.currentTimeMillis() - date.getTime();
+        long hours = Math.round(diff / (60 * 60 * 1000));
+
+        if(hours < 12) {
+            return "less than a day ago";
+        } else {
+            long days = Math.round(diff / (24.0 * 60 * 60 * 1000));
+            if (days == 0)
+                return "today";
+            else if (days == 1)
+                return "yesterday";
+            else if (days == 7)
+                return ((int) (days / 7)) + " week ago";
+            else if (days < 14)
+                return days + " days ago";
+            else if (days <= 27)
+                return ((int) (days / 7)) + " weeks ago";
+            if (days == 28 || days == 29 || days == 30 || days == 31)
+                return ((int) (days / 30)) + " month ago";
+            else if (days < 365)
+                return ((int) (days / 30)) + " months ago";
+            else if (days == 365)
+                return ((int) (days / 365)) + " year ago";
+            else
+                return ((int) (days / 365)) + " years ago";
+        }
+    }
+
+    public static List<String> getDates(List<Date> dates){
+
+        List<String> dateStrings = new ArrayList<>();
+
+        for(Date date : dates){
+
+            dateStrings.add(getDate(date));
+            System.out.println("CHECK " + (getDate(date)));
+        }
+
+        return dateStrings;
+    }
+
+    public static List<Date> getPostDateObjs(List<Post> dates){
+
+        List<Date> dateObjs = new ArrayList<>();
+
+        for (Post post : dates){
+            dateObjs.add(post.getCreationDate());
+        }
+        return dateObjs;
     }
 
 }
